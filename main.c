8b14a7a187a4711ec22e69cef386c7303e871976
matiiezequiiel <matii.ezequiiel@gmail.com>
2020-04-09 21:48:47 -0300

    9/4/2020 21:48

--- main.c
+++ main.c
@@ -3,11 +3,72 @@
 #include "funciones.h"
 
 /*TP 1 PROGRAMACION*/
-
-//1° CAMBIO PRUEBA GIT.
+/*Hacer una calculadora. Para ello el programa iniciará y contará con un menú de opciones:
+1. Ingresar 1er operando (A=x)
+2. Ingresar 2do operando (B=y)
+3. Calcular la suma (A+B)
+4. Calcular la resta (A-B)
+5. Calcular la division (A/B)
+6. Calcular la multiplicacion (A*B)
+7. Calcular el factorial (A!)
+8. Calcular todas las operaciones
+9. Salir
+• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte,
+que contenga las funciones para realizar las 4 operaciones.
+• En el menú deberán aparecer los valores actuales cargados en los operandos A y B
+(donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado)
+• Deberán contemplarse los casos de error (división por cero, etc)
+• Documentar todas las funciones
+*/
 
 int main()
 {
-    printf("Hello world!\n");
+    int opcion;
+    int primerOperando;
+    int segundoOperando;
+
+    do
+    {
+        printf("BIENVENIDOS A LA CALCULADORA!!\n\n");
+        printf("1. Ingresar 1er operando (A=x)");
+        printf("\n2. Ingresar 2do operando (B=y)");
+        printf("\n3. Calcular la suma (A+B)");
+        printf("\n4. Calcular la resta (A-B)");
+        printf("\n5. Calcular la division (A/B)");
+        printf("\n6. Calcular la multiplicacion (A*B)");
+        printf("\n7. Calcular el factorial (A!)");
+        printf("\n8. Calcular todas las operaciones");
+        printf("\n9. Salir");
+
+        printf("\n\nOpcion elegida:");
+        scanf("%d",& opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                printf("Gracias por haber usado esta calculadora!.");
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            case 6:
+            break;
+            case 7:
+            break;
+            case 8:
+            break;
+            case 9:
+                printf("Gracias por haber usado esta calculadora!.");
+            break;
+        }
+
+    }while(opcion!=9);
+
+
     return 0;
 }
